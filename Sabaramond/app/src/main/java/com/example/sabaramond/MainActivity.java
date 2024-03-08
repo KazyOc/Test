42d02bd9ac8ba0c558cf3b48b7435798a2063298
KazyOc <Luisa.neira.m21@gmail.com>
2024-03-08 16:09:39 -0500

    guardar

---
+++ Sabaramond/app/src/main/java/com/example/sabaramond/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sabaramond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtVal1, edtVal2;
+    Button btnCalcular;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtVal1 = findViewById(R.id.edtVal1);
+        edtVal2 = findViewById(R.id.edtVal2);
+        btnCalcular = findViewById(R.id.btnCalcular);
+    }
+}
